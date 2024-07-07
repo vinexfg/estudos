6773c2ae0ce52f8991a03497fc828a5aa0c0a895
vinexfg <136178252+vinexfg@users.noreply.github.com>
2024-07-07 13:39:17 -0300

    a

--- pack_analise/teste_pathlib/teste_pathlib.py
+++ pack_analise/teste_pathlib/teste_pathlib.py
@@ -1,22 +1,36 @@
 
-
-class Produt:
-    def __init__(self, number, name,amount):
+class Product:
+    def __init__(self, number, name, amount):
         self.number = number
         self.name = name
-        self.list = []
         self.amount = amount
+        self.list = []
 
     def data(self):
-        self.list.append(self.name , self.amount)
+        return(self.name, self.amount)
+    
+    def Print_data(self):
+        print(f'Number: {self.number} / Name: {self.name} /  Amount: {self.amount} Unit ')
+
+    def add_list_products(self):
+        self.list.append(self.data())
         return self.list
     
-    def Print(self):
-        print(self.list)
+    def Print_list(self):
+        for item in self.list:
+            return item
 
-    
 
-product1 = Produt(12, 'biscoito', 23)
+Number_product = int(input('Number: '))
+Name_product = input('Name: ')
+Amount_product = int(input('Amount: '))
+print('           ')
+print('           ')
+print('           ')
+print('           ')
 
-product1.Print()
+Product1 = Product(Number_product, Name_product, Amount_product)
 
+Product1.Print_data()
+Product1.add_list_products()
+Product1.Print_list()
