6f3c8e2d091b57e32101d623b09ecf8d31286452
vinexfg <136178252+vinexfg@users.noreply.github.com>
2024-07-25 17:43:40 -0300

    a

--- estudos/estudos2c.py
+++ estudos/estudos2c.py
@@ -6,11 +6,16 @@ from contextlib import contextmanager
 
 @contextmanager
 def my_open(caminho_arquivo, modo):
-    arquivo = open(caminho_arquivo, modo, encoding= 'utf8')
-    yield arquivo
-    print('Fechando arquivo')
-    arquivo.close()
+    try:
+        arquivo = open(caminho_arquivo, modo, encoding= 'utf8')
+        yield arquivo
+    except Exception as e:
+        print('Ocorreu erro', e)
+    finally:
+        print('Fechando arquivo')
+        arquivo.close()
+
     
 
 with my_open('aula689.txt', 'w') as arquivo:
-    arquivo.write('teste1/n')s
+    arquivo.write('teste1/n')
