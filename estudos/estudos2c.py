1d07c53e8ed2b5ef854587589fcd6fbb76dab654
vinexfg <136178252+vinexfg@users.noreply.github.com>
2024-08-01 19:05:52 -0300

    Update estudos2c.py

--- estudos/estudos2c.py
+++ estudos/estudos2c.py
@@ -1,4 +1,14 @@
+"""Game life"""
+import time
 
+class Product:
+    def __init__(self, name, amount, lista):
+        self.name = name
+        self.amount = amount
+        self.lista = lista
+        
+        
 
-
-
+    def data_product(self):
+        # slow_print =
+        pass
