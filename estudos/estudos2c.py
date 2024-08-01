cc8a6a1d469a9f738ce11b19af9b09bbaa48d184
vinexfg <136178252+vinexfg@users.noreply.github.com>
2024-08-01 17:07:38 -0300

    Update estudos2c.py

--- estudos/estudos2c.py
+++ estudos/estudos2c.py
@@ -1,23 +1,4 @@
 
 
 
-class Time:
-    def __init__(self, name):
-        self.name = name
-    
-    def __repr__(self):
-        class_name = self.__class__.__name__
-        class_dict = self.__dict__
-        class_repr = f'{class_name} ({class_dict})'
-        return class_repr
-    
-class Planeta:
-    def __init__(self, name):
-        self.name = name
-
-brasil = Time('Brasil')
-portugal = Time('Portugal')
-
-
-
 
