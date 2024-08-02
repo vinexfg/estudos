052c13943b4ae8d3d91fa3ae612b59a9c0902c61
vinexfg <136178252+vinexfg@users.noreply.github.com>
2024-08-02 18:35:09 -0300

    Update estudos2c.py

--- estudos/estudos2c.py
+++ estudos/estudos2c.py
@@ -1,14 +1,34 @@
-"""Game life"""
-import time
+import pygame
 
-class Product:
-    def __init__(self, name, amount, lista):
+
+"""GAME do meu jeito"""
+
+
+class Person:
+    def __init__(self, name =None, age= None, sex= None, skill= None, level= None):
         self.name = name
-        self.amount = amount
-        self.lista = lista
-        
-        
+        self.age = age
+        self.sex = sex
+        self.skill = skill
+        self.level = level
 
-    def data_product(self):
-        # slow_print =
-        pass
+    def __repr__(self):
+        return (f'Person: (name={self.name} / age={self.age} / sex={self.sex} / '
+                f'skill={self.skill} / level={self.level})')
+    
+
+    def store_data(self):
+        data = []
+        return data
+
+    def get_data(self):
+        self.store_data()
+        questions = [
+            'name',
+            'age',
+            'sex',
+            'skill'
+        ]
+        for quest in questions:
+            response = input(f'Please enter your {quest}: ')
+            self.data
