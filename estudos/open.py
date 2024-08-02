bbe90081d00fb40c77de072286d169af2d3d8917
vinexfg <136178252+vinexfg@users.noreply.github.com>
2024-08-02 19:29:59 -0300

    Update open.py

--- estudos/open.py
+++ estudos/open.py
@@ -1,2 +1,18 @@
-from estudos1 import *
+from estudos1 import Person, Datacollector
 
+
+print('MENU')
+print()
+print()
+print('hoose one of the options: ')
+option_allowed = ['1', '2']
+while True:
+    option = input('[1]ADD (person)  [2]exit')
+
+    if option not in option_allowed:
+        print('number invalid')
+        continue
+    
+    elif option == '1':
+        
+        
