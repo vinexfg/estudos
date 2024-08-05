b7cb1b57dd0f2919589839eb753b50fadf0a2fef
vinexfg <136178252+vinexfg@users.noreply.github.com>
2024-08-05 15:21:12 -0300

    Update contas.py

--- banco/contas.py
+++ banco/contas.py
@@ -25,3 +25,6 @@ class ContaPoupanca(Conta):
         if valor_pos_saque >= 0:
             self.saldo -= valor
             self.detalhes(f'(SAQUE: {valor})')
+
+        print('Não foi possivel sacar o valor desejado')
+        self.detalhes(f'(SAQUE NEGADO: {valor})')
