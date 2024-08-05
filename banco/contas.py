af71970c7d333755a0ae2369726889b8a713f162
vinexfg <136178252+vinexfg@users.noreply.github.com>
2024-08-05 15:20:02 -0300

    a

---
+++ banco/contas.py
@@ -0,0 +1,27 @@
+import abc
+
+class Conta(abc.ABC):
+    def __init__(self, agencia, conta, saldo):
+        self.agencia = agencia
+        self.conta = conta
+        self.saldo = saldo
+    
+    @abc.abstractmethod
+    def sacar(self, valor):
+        pass
+
+    def depositar(self, valor):
+        self.saldo += valor
+        self.detalhes(f'(DEPÓSITO {valor})')
+
+    def detalhes(self, msg=''):
+        print(f' o seu saldo é {self.saldo:.2f} {msg}')
+
+
+class ContaPoupanca(Conta):
+    def sacar(self, valor):
+        valor_pos_saque = self.saldo - valor
+
+        if valor_pos_saque >= 0:
+            self.saldo -= valor
+            self.detalhes(f'(SAQUE: {valor})')
