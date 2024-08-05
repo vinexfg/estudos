9da248b524b4215fd3a4fc85b27c8337de7edcd7
vinexfg <136178252+vinexfg@users.noreply.github.com>
2024-08-05 16:33:04 -0300

    Update pessoas.py

--- banco/pessoas.py
+++ banco/pessoas.py
@@ -18,3 +18,13 @@ class Pessoa:
     @idade.setter
     def idade(self, idade: int):
         self._idade = idade
+
+class Cliente(Pessoa):
+    def __init__(self, nome: str, idade: int) -> None:
+        super().__init__(nome, idade)
+        self.conta = None
+
+if __name__ == '__name__':
+    c1 = Cliente('vinicius', 30 )
+    print(c1)
+    print(c1.conta)
