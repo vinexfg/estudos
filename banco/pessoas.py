4b33c3b909e4132d298184860c310ac3889a02b9
vinexfg <136178252+vinexfg@users.noreply.github.com>
2024-08-05 15:56:21 -0300

    Update pessoas.py

--- banco/pessoas.py
+++ banco/pessoas.py
@@ -1,5 +1,22 @@
 class Pessoa:
     def __init__(self, nome: str, idade: int) -> None:
-        self.nome = nome
-        self.idade = idade
-        
+        self.nome: str = nome
+        self.idade: int = idade
+
+    @property
+    def nome(self):
+        return self._nome
+    
+    @nome.setter
+    def nome(self, nome: str):
+        self._nome = nome
+
+    
+    @property
+    def idade(self):
+        return self._idade
+    
+    @idade.setter
+    def idade(self):
+        return self._idade
+    
