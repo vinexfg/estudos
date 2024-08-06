e9ed67281e622a1306f7ca7c3e6ff156d77309ee
vinexfg <136178252+vinexfg@users.noreply.github.com>
2024-08-06 16:12:59 -0300

    Create banco.py

---
+++ banco/banco.py
@@ -0,0 +1,12 @@
+class Banco:
+    def __init__(
+            self,
+            agencias= None,
+            clientes= None,
+            contas= None,
+    ):
+        self.agencias = agencias
+        self.clientes = clientes
+        self.contas = contas
+
+       
