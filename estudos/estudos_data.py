d149c3f03941737f68164eff2cbb1acba52d3d4f
vinexfg <136178252+vinexfg@users.noreply.github.com>
2024-08-09 20:17:40 -0300

    a

--- estudos/estudos_data.py
+++ estudos/estudos_data.py
@@ -1,11 +1,15 @@
 class Mylist:
     def __init__(self) -> None:
         self._data= {}
+        self.index = 0
 
     def append(self, value):
-        self._data[0] = value
+        self._data[self.index] = value
+        self.index+= 1
 
 if __name__=='__main__':
     lista = Mylist()
     lista.append('maria')
-    print(lista)
+    print(lista._data)
+    lista.append('vinicius')
+    print(lista._data)
