bd7bfea5921fc0ec533b89da6eb17512e7ddc86e
vinexfg <136178252+vinexfg@users.noreply.github.com>
2024-08-09 21:04:14 -0300

    Update datatimeES.py

---
+++ estudos/data_time_estudos.py/datatimeES.py
@@ -0,0 +1,12 @@
+from datetime import datetime
+from pytz import timezone
+
+
+# data_str = "2022-04-20 07:49:23"
+# data_str_fmt = '%Y-%m-%d %H:%M:%S'
+
+# data = datetime.strptime(data_str, data_str_fmt)
+# print(data)
+
+data = datetime.now()
+print(data)
