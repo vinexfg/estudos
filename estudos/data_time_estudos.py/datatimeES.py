13a736dc05839a368faddb365b184ea105c566b7
vinexfg <136178252+vinexfg@users.noreply.github.com>
2024-08-10 11:15:16 -0300

    Update datatimeES.py

--- estudos/data_time_estudos.py/datatimeES.py
+++ estudos/data_time_estudos.py/datatimeES.py
@@ -1,6 +1,8 @@
-from datetime import datetime
+from datetime import datetime, timedelta
 
-fmt =  '%d/%m/%Y %H/%M/%S'
+fmt = '%d/%m/%Y %H:%M:%S'
 
 data_inicio = datetime.strptime('20/04/1987 09:30:30', fmt)
 data_fim = datetime.strptime('12/12/2022 08:20:20', fmt)
+delta = timedelta(days=10, hours=2)
+print(data_fim + delta)
