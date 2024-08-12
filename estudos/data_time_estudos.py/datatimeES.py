03454f4e3941adc87f81f488e67157c527f54247
vinexfg <136178252+vinexfg@users.noreply.github.com>
2024-08-12 11:17:22 -0300

    a

--- estudos/data_time_estudos.py/datatimeES.py
+++ estudos/data_time_estudos.py/datatimeES.py
@@ -1,17 +1,13 @@
-from datetime import datetime
-from dateutil.relativedelta import relativedelta
-from dataclasses import dataclass
-
-@dataclass
-class Loan:
-    amount: float
-    interest_rate: float
-    term_years: int
-    
-    
-    def loan_time(self):
-        total_amount = self.amount * (1 + self.interest_rate * self.term_years)
-        return total_amount
-    
+import os
+caminho = os.path.join('Desktop', 'curso', 'arquivo.txt')
+# print(caminho)
 
 
+# diretorio, arquivo = os.path.split(caminho)
+# caminho_arquivo, extensao_arquivo = os.path.splitext(arquivo)
+# print(caminho_arquivo, extensao_arquivo)
+
+
+
+print(os.path.abspath('.'))
+print(os.path.basename(caminho))
