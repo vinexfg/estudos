85a77e6d20f500bcea0ea7f3e1872b23364fde95
vinexfg <136178252+vinexfg@users.noreply.github.com>
2024-08-12 10:26:31 -0300

    a

--- estudos/data_time_estudos.py/aula_De_python.py
+++ estudos/data_time_estudos.py/aula_De_python.py
@@ -1,7 +1,9 @@
 import calendar
 import locale
+from datetime import datetime
 
-locale.setlocale(locale.LC_ALL, '')
+# locale.setlocale(locale.LC_ALL, '')
 
-print(calendar.monthrange(2022, 12))
-print(list(enumerate(calendar.day_name)))
+# print(calendar.monthrange(2022, 12))
+# print(list(enumerate(calendar.day_name)))
+print(datetime.now())
---
+++ estudos/data_time_estudos.py/teste_js.js
@@ -0,0 +1,5 @@
+console.log('ola tudo bem')
+
+
+let nome = "vinicius";
+console.log(`${nome}, ola`)
