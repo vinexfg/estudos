af4207c59d90248a5424f88fbacd4a37cf0d2628
vinexfg <136178252+vinexfg@users.noreply.github.com>
2024-08-13 19:33:51 -0300

    Update pathlib_teste.py

--- oss_jsons.py/pathlib_teste.py
+++ oss_jsons.py/pathlib_teste.py
@@ -1,6 +1,8 @@
-import pathlib
+from pathlib import Path
 import os
 
 
+caminho_projeto = Path()
+# descobrir_diretorio = os.getcwd()
 
-descobrir_diretorio = os.getcwd()
+print(caminho_projeto.absolute())
