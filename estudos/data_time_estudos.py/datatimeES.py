d10bff8abe4da90b1b435a72f39c25f3fcb27029
vinexfg <136178252+vinexfg@users.noreply.github.com>
2024-08-13 17:26:46 -0300

    a

--- estudos/data_time_estudos.py/datatimeES.py
+++ estudos/data_time_estudos.py/datatimeES.py
@@ -1,11 +1,31 @@
 import os
+import shutil
 
 
+os.system('cls')
 caminho = os.path.join('C:\\Users\\vinys\\OneDrive\\Tarefas\\estudos')
-print(caminho)
+# print(caminho)
 
 
 for item in os.listdir(caminho):
-    print(item) 
+    # print(item)
+    pass
 
 diretorio_atual = 'C:\\Users\\vinys\\OneDrive\\Tarefas\\estudos'
+
+
+descobrir_diretorio = os.getcwd()
+
+
+HOME = os.path.expanduser('~')
+DESKTOP = os.path.join(HOME, 'Desktop')
+PASTA_ORIGINAL = os.path.join(DESKTOP, 'EXEMPLO')
+nova_pasta = os.path.join(DESKTOP, 'nova_pasta')
+# print(nova_pasta)
+
+
+os.makedirs('nova_pasta')
+for root, dirs, files in os.walk(PASTA_ORIGINAL):
+    for file in files:
+        print(file)
+
--- estudos/man_game.py
+++ estudos/man_game.py
@@ -1,34 +1,10 @@
-import time
-
-# Função para impressão lenta do texto
-def slow_print(text, delay=0.05):
-    for char in text:
-        print(char, end='', flush=True)
-        time.sleep(delay)
-    print()
-
-def main():
-    slow_print("Bem-vindo ao jogo das emoções!")
-    time.sleep(1)
-    slow_print("Vamos começar com algumas perguntas que podem mexer com seus sentimentos.")
-    time.sleep(1)
-
-    questions = [
-        "Qual é sua lembrança mais feliz?",
-        "Se pudesse mudar algo no passado, o que seria?",
-        "Qual é seu maior medo?",
-        "Qual foi o momento mais difícil que já enfrentou?",
-        "O que te faz sentir mais vivo?"
-    ]
-
-    for question in questions:
-        slow_print(question)
-        answer = input("> ")
-        slow_print("Obrigado por compartilhar.\n")
-        time.sleep(1)
-
-    slow_print("Obrigado por jogar! Esperamos que você tenha aproveitado essa experiência.")
-    time.sleep(1)
-
-if __name__ == "__main__":
-    main()
+string_json = '''
+{
+  "title": "O Senhor dos Anéis: A Sociedade do Anel",
+  "original_title": "The Lord of the Rings: The Fellowship of the Ring",
+  "is_movie": true,
+  "imdb_rating": 8.8,
+  "year": 2001,
+  "characters": ["Frodo", "Sam", "Gandalf", "Legolas", "Boromir"],
+  "budget": null
+} '''
