3e5ad824f63cd5cff17597c7b7b8014a67af6444
vinexfg <136178252+vinexfg@users.noreply.github.com>
2024-08-15 17:46:11 -0300

    a

--- oss_jsons.py/pathlib_teste.py
+++ oss_jsons.py/pathlib_teste.py
@@ -1,21 +1,5 @@
-from pathlib import Path
-import csv
+import random
 
-CAMINHO_CSV = Path(__file__).parent / 'pathlib_teste.csv'
+r_range = random.randrange(10, 20 , 2)
 
-
-lista_clientes = [
-    {'Nome': 'Luiz Otávio', 'Endereço': 'Av 1, 22'},
-    {'Nome': 'João Silva', 'Endereço': 'R. 2, "1"'},
-    {'Nome': 'Maria Sol', 'Endereço': 'Av B, 3A'},
-]
-
-with open(CAMINHO_CSV, 'w') as arquivo:
-    colunas = (lista_clientes[0].keys())
-    escritor = csv.writer(arquivo)
-
-    escritor.writerow(colunas)
-
-    for cliente in lista_clientes:
-        print(cliente.values())
-        
+print(r_range)
