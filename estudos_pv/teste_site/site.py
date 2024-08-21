f3d49a16af51d2d7b1c24021a49ae40ebcb9cebe
vinexfg <136178252+vinexfg@users.noreply.github.com>
2024-08-21 19:38:24 -0300

    a

--- estudos_pv/teste_site/site.py
+++ estudos_pv/teste_site/site.py
@@ -1,7 +1,11 @@
 import requests
+from bs4 import BeautifulSoup
 
-url = 'http://localhost:3001/teste_site/#footer' 
+url = 'http://localhost:3001/estudos_pv/#footer'
 response = requests.get(url)
-print(response.status_code)
-# print(response.headers)
-print(response.text)
+raw_html = response.text
+parsed_html = BeautifulSoup(raw_html, 'html.parsed')
+
+
+top_jobs_heading = parsed_html.select_one('#intro > div > div > article > h2')
+print(top_jobs_heading)
