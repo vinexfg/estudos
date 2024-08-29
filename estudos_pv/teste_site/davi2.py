5f65d66c3a3b2c562c3d0fc2b32a8b80bce35c58
vinexfg <136178252+vinexfg@users.noreply.github.com>
2024-08-28 22:31:23 -0300

    Update davi2.py

--- estudos_pv/teste_site/davi2.py
+++ estudos_pv/teste_site/davi2.py
@@ -1,7 +1,22 @@
-numero1 = 1
-numero2 = 2
 
-if numero1 > numero2:
-    print('Verdadeiro')
-else:
-    print('Falso')
+
+class Passaword:
+    def __init__(self):
+        self.caracteres_permitidos = 25
+        self.minimo_de_caracteres = 8
+        self.caracteres_especias = ['!', '@', '#', '%', '&', '*']
+
+    def check_caracteres(self, passaword):
+        if len(passaword) > self.caracteres_permitidos:
+            print('Sua senha excedeu a quantidade de caracteres permitidos')
+            return False
+        elif len(passaword) < self.minimo_de_caracteres:
+            print('a senha deve ter no minimo 7 caracteres')
+        else:
+            return True
+        
+        
+    def check_minimo(self, passaword):
+        if not any(char in passaword for char in self.caracteres_especias):
+            print('A senha deve conter pelo menos um caractere especial: !, @, #, %, &, *')
+
